b2465cf361b0556381be1a57fc1a68d601607fa3
Alvaro Ollero <aollero@gmail.com>
2021-08-30 16:34:30 +0300

    fixing make run rule so that it builds its dependencies

--- srcs/target/test.mk
+++ srcs/target/test.mk
@@ -3,10 +3,10 @@ TEST0 := $(BLD_DIR)/utest_app
 $(TEST0) : $(OBJ_DIRS) $(OBJ)
 	make -C srcs/app/test
 
-.PHONY: run
-run:
-	$(BLD_DIR)/utest_app
-
 .PHONY: test
 test:
 	make $(TEST0)
+
+.PHONY: run
+run: $(TEST0)
+	$(TEST0)
