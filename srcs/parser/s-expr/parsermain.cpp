05f7938f8a11a86e9817c017a30d1d0a08c75b47
Alvaro Ollero <aollero@gmail.com>
2021-09-05 00:15:15 +0300

    some more cleanup

--- srcs/parser/s-expr/parsermain.cpp
+++ srcs/parser/s-expr/parsermain.cpp
@@ -1,13 +1,17 @@
 #include "Parser.hpp"
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+	if (argc < 2)
+		return 1;
 	try {
-	Parser parser(argv[1]);
-	std::list<std::string> tokens = parser.tokens_;
-	std::list<std::string>::iterator it = tokens.begin();
-	for (; it != tokens.end(); ++it)
-		std::cout << *it << "\n";
-	} catch(const std::exception &e){ std::cerr << e.what() << std::endl; }
+		Parser parser(argv[1]);
+		std::list<std::string> *tokens = parser.tokens_;
+		std::list<std::string>::iterator it = tokens->begin();
+		for (; it != tokens->end(); ++it) {
+			std::cout << *it << "\n";
+		}
+	} catch(const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return 0;
 }
