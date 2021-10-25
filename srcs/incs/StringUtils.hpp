cc5523b540a782c49121c4c13105a2048e2575c0
Alvaro Ollero <aollero@gmail.com>
2021-10-25 23:09:44 +0200

    CGI funcionagit status

--- srcs/incs/StringUtils.hpp
+++ srcs/incs/StringUtils.hpp
@@ -1,8 +1,10 @@
 #ifndef SRCS_INCS_STRINGUTILS_HPP_
 #define SRCS_INCS_STRINGUTILS_HPP_
 #include <string>
+#include <stdint.h>
 
 std::string	TrimString(const std::string &str, const std::string &trim_chars);
 std::string	ToLowerString(std::string str);
+std::string UInt16ToStr(uint16_t nbr);
 
 #endif  // SRCS_INCS_STRINGUTILS_HPP_
